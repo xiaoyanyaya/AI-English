7d0a55cef6aa3d90b688b7235889da8da8ebd3f2
chenziyu <2310240409@qq.com>
2024-07-08 10:29:19 +0800

    feat:自动化

--- src/configs/env.js
+++ src/configs/env.js
@@ -3,7 +3,22 @@ const proDomain = 'https://wapi.aien.xiaolixb.com/v1' // 生产环境
 
 const ENV = 'pro' // dev -测试 pro -生产
 
-const apiDomain = ENV === 'pro' ? proDomain : devDomain
+// #ifdef MP-WEIXIN
+let path = "";
+// ---------------- 根据微信开发环境配置请求地址 --------------------
+// 获取当前帐号信息
+const accountInfo = wx.getAccountInfoSync();
+console.log("accountInfo", accountInfo)
+// env类型 develop:开发版、trial:体验版、release:正式版
+const envWx = accountInfo.miniProgram.envVersion;
+if(envWx === 'release'){
+	path = proDomain
+}else{
+	path = devDomain
+}
+// #endif
+
+const apiDomain = path
 export {
   ENV,
   apiDomain,

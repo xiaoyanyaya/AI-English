c0b0f5946e4037df787731f486d01341e48c202f
chenziyu <2310240409@qq.com>
2024-08-14 01:17:16 +0800

    1

---
+++ src/pages/me/apply/cooperative.vue
@@ -0,0 +1,195 @@
+<template>
+  <view class="body" :style="bodyStyle">
+    <cy-navbar showBack bgColor="transparent" textColor="#3D3D3D">
+      <view class="t-size-30">申请成为合作商</view>
+    </cy-navbar>
+
+    <view class="content">
+      <view class="username-title ml-2">
+        <span class="t-color-DC0C0C t-size-32 must-icon">*</span>
+        <span class="font-weight-bold t-size-36 ml-2">姓名</span>
+      </view>
+      <view class="username-box mt-2 flex align-item-center px-4">
+        <input v-model="params.realname" class="flex-1" type="text" placeholder="请输入姓名"/>
+      </view>
+
+      <view class="username-title ml-2 mt-10">
+        <span class="t-color-DC0C0C t-size-32 must-icon">*</span>
+        <span class="font-weight-bold t-size-36 ml-2">手机号</span>
+      </view>
+      <view class="username-box mt-2 flex align-item-center px-4">
+        <input v-model="params.phone" class="flex-1" type="number" placeholder="手机号"/>
+      </view>
+
+      <view class="footer ml-5 mt-7">
+        <view class="select">
+          <view @click="isAuthorize = !isAuthorize"
+                class="circle flex align-item-center justify-content-center"
+                :class="isAuthorize ? 'c-active' : ''">
+            <u-icon v-if="isAuthorize" name="checkbox-mark" size="20" color="#FFFFFF"></u-icon>
+          </view>
+        </view>
+        <span class="ml-2 t-color-8A8A8A">阅读并同意</span>
+        <span class="text" @click="toUserPage(0, '用户协议')">《用户推广协议》</span>
+      </view>
+
+      <view class="flex align-item-center justify-content-center">
+        <view class="btn-box flex align-item-center justify-content-center t-size-32 t-color-fff" @click="partnerApply">
+            立即申请
+        </view>
+      </view>
+    </view>
+
+    <u-modal
+      :width="550"
+      :mask-close-able="true"
+      :show-confirm-button="false"
+      :show-title="false"
+      v-model="showModel">
+      <view class="slot-content">
+        <view class="t-color-8A8A8A t-size-30 review">已阅读并同意《用户注册协议》《隐私政策》</view>
+        <view class="flex align-item-center justify-content-around t-size-24 font-weight-bold mt-4">
+          <view class="t-color-8A8A8A" @click="showModel = false">不同意</view>
+          <view @click="clickAllow"
+                class="t-color-fff allow flex align-item-center justify-content-center">
+            同意并申请
+          </view>
+        </view>
+      </view>
+    </u-modal>
+  </view>
+</template>
+
+<script>
+import MyMixin from "../../../utils/MyMixin";
+import {partnerApply} from "../../../api/me";
+
+export default {
+  mixins: [MyMixin],
+  data() {
+    return {
+      isAuthorize: false,
+      params: {
+        realname: '',
+        phone: ''
+      },
+      showModel: false,
+      message: '',
+      messageDate: ''
+    };
+  },
+  computed: {
+    bodyStyle() {
+      return  `background-image: url(${this.imageBaseUrl}/8-13-03.png);
+              background-size: 100% 100%;`
+    }
+  },
+  methods: {
+    clickAllow() {
+      this.isAuthorize = true;
+      this.showModel = false;
+      this.partnerApply();
+    },
+    partnerApply() {
+      if (!this.isAuthorize) {
+        return this.showModel = true;
+      }
+
+      partnerApply(this.params).then(res => {
+        console.log(res);
+        if (res.data.code === 200) {
+          this.message = res.data.message
+          this.messageDate = res.data.result
+          uni.showToast({
+            title: '申请成功',
+            icon: 'success',
+            duration: 1500
+          });
+          setTimeout(() => {
+            this.$navigateTo(`/pages/me/apply/applySuccess?message=${this.message}&messageDate=${this.messageDate}`);
+          }, 1500);
+        }
+      });
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.body {
+  min-height: 100vh;
+}
+
+.content {
+  padding: 120rpx 70rpx 0 70rpx;
+
+  .username-title {
+
+  }
+
+  .username-box {
+    height: 100rpx;
+    border-radius: 100rpx;
+    background: #ffffff;
+  }
+}
+
+.must-icon {
+  margin-top: -10rpx;
+  position: absolute;
+}
+
+.footer {
+  display: flex;
+  justify-content: flex-start;
+
+  img {
+    width: 32rpx;
+    height: 32rpx;
+  }
+
+  p {
+    color: #999999;
+    font-size: 24rpx;
+  }
+
+  .text {
+    color: #1863E5;
+    font-size: 24rpx;
+  }
+}
+.slot-content {
+  padding: 40rpx;
+
+  .review {
+    line-height: 40rpx;
+  }
+
+  .allow {
+    background: #2b55a0;
+    width: 200rpx;
+    height: 64rpx;
+    border-radius: 30rpx;
+  }
+}
+.circle {
+  width: 32rpx;
+  height: 32rpx;
+  border-radius: 50%;
+  border: 1rpx solid #8A8A8A;
+  margin-top: 4rpx;
+}
+
+.c-active {
+  background: #3A73D9;
+  border: 1rpx solid #3A73D9;
+}
+
+.btn-box {
+  width: 450rpx;
+  height: 80rpx;
+  border-radius: 50rpx;
+  background: #3A73D9;
+  margin-top: 150rpx;
+}
+</style>
--- src/pages/me/order.vue
+++ src/pages/me/order.vue
@@ -1,26 +1,32 @@
 <template>
-  <view class="main-body pb-5">
-    <cy-navbar show-back>
+  <view class="body pb-5" :style="bodyStyle">
+    <cy-navbar showBack bgColor="transparent" textColor="#3D3D3D">
       <view class="t-size-30">推广订单</view>
     </cy-navbar>
 
     <view class="px-3">
       <view class="px-3 pt-3 mt-4 pb-4 bg-white radio-10"
             v-for="(data, dataIndex) in orderContent" :key="dataIndex">
-        <view class="flex justify-content-between">
-          <view class="flex align-item-end">
+        <view class="flex justify-content-between align-item-center">
+          <view class="flex align-item-center">
             <image :src="data.userInfo.avatar" class="avatar mr-2"/>
-            <view class="t-size-30">{{ data.userInfo.nickName }}</view>
-            <view class="t-size-26 t-color-8A8A8A">(MID:{{ data.userInfo.memberNo }})</view>
+            <view class="flex align-item-end">
+              <view class="t-size-30">{{ data.userInfo.nickName }}</view>
+              <view class="t-size-22 t-color-8A8A8A"> (MID:{{ data.userInfo.memberNo }})</view>
+            </view>
+          </view>
+          <view class="t-color-1863E5 mr-3">
+            已付款
           </view>
         </view>
 
-        <view class="mt-4 order-content px-3 pt-1 pb-3 t-size-30 t-color-6a6a6a">
-          <view v-for="(item, index) in data.data" :key="index">
+        <view class="mt-4 order-content px-3 pt-1 t-size-30 t-color-6a6a6a">
+          <view v-for="(item, index) in data.data" :key="index"
+                :class="index === data.data.length - 1 ? '' : 'item'">
             <view class="flex align-item-center justify-content-between mt-2">
               <view>{{ item.title }}</view>
               <view v-if="index === 3" class="flex align-item-center recomment">
-                <image :src="item.avatar" class="avatar mr-1"/>
+<!--                <image :src="item.avatar" class="avatar mr-1"/>-->
                 <view class="t-size-28">{{ getNameWithEllipsis(item.nickName, 10) }}</view>
               </view>
               <view v-else :class="index === 4 ? 't-color-DC0C0C' : ''">
@@ -43,6 +49,12 @@ import {orderList} from "../../api/me";
 
 export default {
   mixins: [MyMixin],
+  computed: {
+    bodyStyle() {
+      return `background-image: url(${this.imageBaseUrl}/8-13-03.png);
+              background-size: 100% 100%;`
+    }
+  },
   data() {
     return {
       isModeData: true,
@@ -104,13 +116,21 @@ export default {
 </script>
 
 <style lang="scss">
+.body {
+  min-height: 100vh;
+}
+
 .order-content {
-  background: #e7e7e7;
+
+  .item {
+    padding-bottom: 14rpx;
+    border-bottom: 1rpx solid rgba(216, 216, 216, 0.3);
+  }
 }
 
 .avatar {
-  width: 50rpx;
-  height: 50rpx;
+  width: 60rpx;
+  height: 60rpx;
   border-radius: 50%;
 }
 </style>

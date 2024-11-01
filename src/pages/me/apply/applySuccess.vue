e38f369209775b008b2d7211a3dfb54cb50505a3
wjy <2670718761@qq.com>
2024-11-01 09:05:57 +0800

    1

--- src/pages/me/apply/applySuccess.vue
+++ src/pages/me/apply/applySuccess.vue
@@ -3,23 +3,35 @@
     <cy-navbar
       @customBack="returnCustom"
       customBack
-      showBack bgColor="transparent" textColor="#3D3D3D">
+      showBack
+      bgColor="transparent"
+      textColor="#3D3D3D"
+    >
       <view class="t-size-30">申请成为合作商</view>
     </cy-navbar>
 
-    <view class="flex align-item-center justify-content-center flex-direction-column">
-      <image :src="`${imageBaseUrl}/8-13-06.png`" mode="widthFix" class="main-image mb-5"></image>
+    <view
+      class="flex align-item-center justify-content-center flex-direction-column"
+    >
+      <image
+        :src="`${imageBaseUrl}/8-13-06.png`"
+        mode="widthFix"
+        class="main-image mb-5"
+      ></image>
 
-      <view class="t-color-3D3D3D t-size-36">{{message}}</view>
-      <view class="t-color-8A8A8A t-size-26 mt-2">{{messageDate}} 提交</view>
+      <view class="t-color-3D3D3D t-size-36">{{ message }}</view>
+      <view class="t-color-8A8A8A t-size-26 mt-2">{{ messageDate }} 提交</view>
 
-<!--      <view class="flex align-item-center justify-content-center">
+      <!--      <view class="flex align-item-center justify-content-center">
         <view class="btn-box-01 flex align-item-center justify-content-center t-size-32 t-color-fff">
           进入分销中心
         </view>
       </view>-->
       <view class="flex align-item-center justify-content-center">
-        <view class="btn-box-02 flex align-item-center justify-content-center t-size-32 t-color-fff" @click="clickBtn">
+        <view
+          class="btn-box-02 flex align-item-center justify-content-center t-size-32 t-color-fff"
+          @click="clickBtn"
+        >
           进入分销中心
         </view>
       </view>
@@ -35,13 +47,13 @@ export default {
   computed: {
     bodyStyle() {
       return `background-image: url(${this.imageBaseUrl}/8-13-03.png);
-              background-size: 100% 100%;`
-    }
+              background-size: 100% 100%;`;
+    },
   },
   data() {
     return {
-      message: '',
-      messageDate: '',
+      message: "",
+      messageDate: "",
     };
   },
   onLoad({ message, messageDate }) {
@@ -51,14 +63,16 @@ export default {
   methods: {
     returnCustom() {
       uni.switchTab({
-        url: '/pages/me/index'
-      })
+        url: "/pages/me/index",
+      });
     },
     clickBtn() {
-      this.$navigateTo("/pages/me/distribution")
-    }
+      uni.switchTab({
+        url: "/pages/me/index",
+      });
+    },
   },
-}
+};
 </script>
 
 <style lang="scss">
@@ -71,19 +85,20 @@ export default {
   margin-top: 200rpx;
 }
 
-.btn-box-01, .btn-box-02 {
+.btn-box-01,
+.btn-box-02 {
   width: 450rpx;
   height: 80rpx;
   border-radius: 50rpx;
 }
 .btn-box-01 {
-  background: #1863E5;
+  background: #1863e5;
   color: #fff;
 }
 .btn-box-02 {
   background: #ffffff;
-  border: 1px solid #1863E5;
-  color: #1863E5;
+  border: 1px solid #1863e5;
+  color: #1863e5;
   margin-top: 130rpx;
 }
 </style>
